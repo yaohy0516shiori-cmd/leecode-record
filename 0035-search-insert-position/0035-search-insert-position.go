6a776ea822dc264a1d0e732b942cecfdde440e97
yaohy0516shiori <yaohy0516shiori@gmail.com>
2026-04-01 17:30:44 +0800

    Time: 0 ms (100%), Space: 4.7 MB (99.57%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.go
@@ -0,0 +1,15 @@
+func searchInsert(nums []int, target int) int {
+    l,r:=0,len(nums)-1
+    for l<=r{
+        mid:=(l+r)/2
+        if nums[mid]==target{
+            return mid
+        }
+        if nums[mid]>target{
+            r=mid-1
+        }else{
+            l=mid+1
+        }
+    }
+    return max(l,r)
+}
