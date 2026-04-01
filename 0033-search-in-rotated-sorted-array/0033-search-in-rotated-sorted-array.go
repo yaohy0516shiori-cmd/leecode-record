080ae442e7188740d98a404c86798d75bf5a2522
yaohy0516shiori <yaohy0516shiori@gmail.com>
2026-04-01 17:24:43 +0800

    Time: 0 ms (100%), Space: 4.3 MB (74.61%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.go
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.go
@@ -2,24 +2,34 @@ func search(nums []int, target int) int {
     if len(nums)==0{
         return -1
     }
+    n:=len(nums)
     l,r:=0,len(nums)-1
+    for l<r{
+        mid:=(l+r)/2
+        if nums[mid]>nums[r]{
+            l=mid+1
+        }else{
+            r=mid
+        }
+    
+    }
+    point:=l
+    if nums[point]<=target && target<=nums[n-1]{
+        return binary(nums,point,n-1,target)
+    }
+    return binary(nums,0,point-1,target)
+}
+
+func binary(nums []int, l int, r int, target int) int{
     for l<=r{
         mid:=(l+r)/2
         if nums[mid]==target{
             return mid
         }
-        if nums[0]<=nums[mid]{
-            if nums[0]<=target && target<nums[mid]{
-                r=mid-1
-            }else{
-                l=mid+1
-            }
+        if nums[mid]<target{
+            l=mid+1
         }else{
-            if nums[mid]<target && target<=nums[len(nums)-1]{
-                l=mid+1
-            }else{
-                r=mid-1
-            }
+            r=mid-1
         }
     }
     return -1
