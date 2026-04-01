0b009377ca66d3091b776fbc8f24c9f34775c60c
yaohy0516shiori <yaohy0516shiori@gmail.com>
2026-04-01 16:20:15 +0800

    Time: 1 ms (58.11%), Space: 19.4 MB (86.61%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,7 +1,7 @@
-# O(m+n)
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        sums=len(nums1)+len(nums2)
+    def findMedianSortedArrays(self, a: List[int], b: List[int]) -> float:
+        # O(m+n)
+        '''sums=len(nums1)+len(nums2)
         i=0
         j=0
         pre,cur=0,0 # doesn't need to store the whole array
@@ -17,5 +17,27 @@ class Solution:
         if sums%2==0:
             return (pre+cur)/2
         else:
-            return cur
+            return cur'''
+        if len(a)>len(b):
+            a,b=b,a
+        m,n=len(a),len(b)
+        left,right=0,m
+        half=(m+n+1)//2 # odd: (m+n+1)//2, not index, even: (m+n)//2==(m+n+1)//2
+        while left<=right:
+            i=(left+right)//2
+            j=half-i
 
+            aleft=a[i-1] if i>0 else float('-inf')
+            aright=a[i]  if i<m else float('inf')
+            bleft=b[j-1] if j>0 else float('-inf')
+            bright=b[j]  if j<n else float('inf')
+
+            if aleft<=bright and bleft<=aright:
+                if not (m+n)%2:
+                    return (max(aleft,bleft)+min(aright,bright))/2
+                else: 
+                    return max(aleft,bleft)
+            if aleft>bright:
+                right=i-1 # use left window
+            else:
+                left=i+1 # use right window
