2696e1ee6aa1a169add7bc64d720e711f2fb539c
yaohy0516shiori <yaohy0516shiori@gmail.com>
2026-04-06 13:50:41 +0800

    Time: 0 ms (100%), Space: 4.8 MB (96.49%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.go
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.go
@@ -6,28 +6,41 @@
  *     Right *TreeNode
  * }
  */
+
 func flatten(root *TreeNode)  {
-    if root==nil || (root.Left==nil && root.Right==nil){
-        return 
-    }
-    queue:=[]*TreeNode{}
+    // if root==nil || (root.Left==nil && root.Right==nil){
+    //     return 
+    // }
+    // queue:=[]*TreeNode{}
+    // var dfs func(node *TreeNode)
+    // dfs=func(node *TreeNode){
+    //     if node==nil{
+    //         return
+    //     }
+    //     queue=append(queue,node)
+    //     dfs(node.Left)
+    //     dfs(node.Right)
+    //     return
+    // }
+    // dfs(root)
+    // for i:=0;i<len(queue);i++{
+    //     queue[i].Left=nil
+    //     if i!=len(queue)-1{
+    //         queue[i].Right=queue[i+1]
+    //     }
+        
+    // }
+    var prev *TreeNode = nil
     var dfs func(node *TreeNode)
     dfs=func(node *TreeNode){
         if node==nil{
             return
         }
-        queue=append(queue,node)
-        dfs(node.Left)
         dfs(node.Right)
-        return
+        dfs(node.Left)
+        node.Right=prev
+        node.Left=nil
+        prev=node
     }
     dfs(root)
-    for i:=0;i<len(queue);i++{
-        queue[i].Left=nil
-        if i!=len(queue)-1{
-            queue[i].Right=queue[i+1]
-        }
-        
-    }
-    return 
 }
