a7727fe2a1a56f0045a7e77c3c7b76f61417aa9a
yaohy0516shiori <yaohy0516shiori@gmail.com>
2026-04-07 19:57:14 +0800

    Time: 0 ms (100%), Space: 6.8 MB (46.38%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.go
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func pathSum(root *TreeNode, targetSum int) int {
+    res:=map[int]int{0:1}
+    ans:=0
+    var dfs func(node *TreeNode, prefix int)
+    dfs=func(node *TreeNode, prefix int){
+        if node==nil{
+            return
+        }
+        prefix+=node.Val
+        ans+=res[prefix-targetSum]
+        res[prefix]+=1
+        dfs(node.Left,prefix)
+        dfs(node.Right,prefix)
+        res[prefix]-=1
+    }
+    dfs(root,0)
+    return ans
+}
