c281428dda6d9e8902909e0b9b7d53d333f8fd65
yaohy0516shiori <yaohy0516shiori@gmail.com>
2026-04-08 14:23:04 +0800

    Time: 2 ms (58.93%), Space: 8.4 MB (14.73%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.go
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.go
@@ -1,11 +1,14 @@
 func longestValidParentheses(s string) int {
+    // use stack, if () at the beignning, ) locates in 1st position, ans calculate the length = 1-(-1)
     res:=[]int{-1}
     ans:=0
     for i:=0;i<len(s);i++{
+        // only store ( position, and pop up when meet )
         if s[i]=='('{
             res=append(res,i)
         }else{
             res=res[:len(res)-1]
+            // if stack is nil, means )'s position is the new beignning
             if len(res)==0{
                 res=append(res,i)
             }else{
