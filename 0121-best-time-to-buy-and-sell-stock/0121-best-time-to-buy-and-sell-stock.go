5c8b472c6d5b661ed2a72249c55360b938e92122
yaohy0516shiori <yaohy0516shiori@gmail.com>
2026-04-13 14:01:35 +0800

    Time: 0 ms (100%), Space: 9.9 MB (56.78%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.go
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.go
@@ -1,6 +1,6 @@
 func maxProfit(prices []int) int {
     profit:=0
-    buy:=prices[0]
+    buy:=prices[0] // recording the lowest price
     for i:=1;i<len(prices);i++{
         if prices[i]<buy{
             buy=prices[i]
