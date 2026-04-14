163ae6347109b5671441cdac2ff4c86d8f58e41d
yaohy0516shiori <yaohy0516shiori@gmail.com>
2026-04-14 15:05:16 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.go
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.go
@@ -1,11 +1,18 @@
 func subarraySum(nums []int, k int) int {
-    res:=map[int]int{0:1}
+    /*
+    [1,2,3,4,5,5,7] k=10
+    prefix[1,3,6,10,15,20,28]
+    if prefix-k in hashmap, means there is an subarray sum==k
+    if not, there is not subarray
+    when we need subarray, prefix is a very good choice
+    */
+    res:=make(map[int]int{0:1})
     prefix:=0
     ans:=0
-    for _, num := range nums{ // equal to i:=0;i<len(nums);i++
-        prefix+=num
-        ans+=res[prefix-k] //res[0]=1, or we cannot count prefix=k
+    for i := range nums{
+        prefix+=i
+        ans+=res[prefix-k]
         res[prefix]++
     }
-    return ans
+    return ans 
 }
