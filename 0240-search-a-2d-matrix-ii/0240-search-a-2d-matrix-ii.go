6a09b78247b21fcf987ed2932b9d18a0fb3cb63e
yaohy0516shiori <yaohy0516shiori@gmail.com>
2026-04-20 15:44:54 +0800

    Time: 16 ms (54.44%), Space: 8.4 MB (20.12%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.go
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.go
@@ -1,8 +1,5 @@
 func searchMatrix(matrix [][]int, target int) bool {
     m,n:=len(matrix),len(matrix[0])
-    if target> matrix[m-1][n-1] || target< matrix[0][0]{
-        return false
-    }
     if m==0 || n==0{
         return false
     }
