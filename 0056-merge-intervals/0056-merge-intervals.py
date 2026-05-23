6420014d6d3bfa824aa8f1c6f34bc115f03c8b9b
yaohy0516shiori <yaohy0516shiori@gmail.com>
2026-05-23 19:23:52 +0800

    Time: 9 ms (33.73%), Space: 23.2 MB (28.23%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -12,7 +12,20 @@ class Solution:
                 end=intervals[i][1]
                 continue
             else:
-                start=min(start,intervals[i][0])
                 end=max(end,intervals[i][1])
         res.append([start,end])
         return res
+
+# class Solution:
+#     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+#         intervals.sort(key=lambda x: x[0])
+
+#         res = []
+
+#         for start, end in intervals:
+#             if not res or res[-1][1] < start:
+#                 res.append([start, end])
+#             else:
+#                 res[-1][1] = max(res[-1][1], end)
+
+#         return res
